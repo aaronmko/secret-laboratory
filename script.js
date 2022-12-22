f2ff4233495e31590033b048e759bfc7cffe04d9
Aaron Ko <minkiko@gmail.com>
2022-12-22 15:44:53 -0600

    descriptions added to js script describing coding lines

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Random Password Generator to generate a password meeting a criteria between 8-128 characters, where the random password can contain numbers, special characters and upper and lower case letters
-
 //Assignment Code + Event Listener to prompt questions when button pushed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Assigning arrays to include numbers, special characters, lowercase letters, and uppercase letters
+// Assigment of variables of numbers special characters, lowercase letters, and uppercase letters
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", ",$", "%", "^", "&", "*", "(", ")", "+", "=","?"];
+var specialCharacters = ["!", "@", "#", ",$", "%", "^", "&", "*", "(", ")", "+", "=","?"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Variables
+// Variables beign used. var confirmLength is an empty string so that it does not result as undefined
 var confirmLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
@@ -27,13 +25,14 @@ function generatePassword() {
       } 
 
       // Alert confirming how many characters the password will have 
-      alert(`Your password will have ${confirmLength} characters`);
+      alert('Your password will have' + confirmLength + 'characters');
 
-    // Determine parameters of password 
+    // Determine parameters of password where you click to confirm the criteria or cancel if you do not want a specific criteria
     var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
     var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
     var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
     var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+
       // Loop if answer is outside the parameters 
       while(!confirmUpperCase === true && !confirmLowerCase === true && !confirmSpecialCharacter === true && !confirmNumericCharacter === true) {
         alert("You must choose at least one parameter");
@@ -43,11 +42,12 @@ function generatePassword() {
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
 
-      // Assign an action to the password parameters
+      // var characters in an empty array for value does not return undefined
       var characters = []
-      
+    
+    // if statements with concatenations of each variable 
     if (confirmSpecialCharacter) {
-      characters = characters.concat(specialChar)
+      characters = characters.concat(specialCharacters)
     }
 
     if (confirmNumericCharacter) {
@@ -64,8 +64,7 @@ function generatePassword() {
 
       console.log(characters)
 
-      // Empty string to be filled based on for loop selecting random characters from the array
-      var randomPassword = ""
+      // Empty string to be that will be filled from the loop where it selects random characters in the array from lines 69-73
       
       for (var i = 0; i < confirmLength; i++) {
         randomPassword = randomPassword + characters[Math.floor(Math.random() * characters.length)];
@@ -74,7 +73,7 @@ function generatePassword() {
       return randomPassword;
 }
 
-// Write password to the #password input
+// This will write the password to the ID password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
